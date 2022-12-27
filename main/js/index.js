e719ead6df3b0719ed6b3561ecc430ad2af1544f
tineodev <tineo.dev@gmail.com>
2022-12-27 17:39:42 -0500

    Add user filter method get

--- main/js/index.js
+++ main/js/index.js
@@ -2,23 +2,8 @@ const html_payments = document.querySelector("#htmlPayments");
 htmlExpired;
 const html_expired = document.querySelector("#htmlExpired");
 
-// async function apiPayments() {
-//   try {
-//     const url = "http://127.0.0.1:8000/payments/";
-//     const response = await fetch(url);
-//     const data = await response.json();
 
-//     console.log(data.results);
-
-//     data.results.forEach((record) => {
-//       html_payments.innerHTML += listResults(record);
-//     });
-//   } catch (error) {
-//     console.log(error);
-//   }
-// }
-
-async function apiGET(pm_element, pm_service, pm_function) {
+async function apiGET(pm_element, pm_service, pm_function, pm_user) {
   try {
     const url = `http://127.0.0.1:8000/${pm_service}/`;
     const response = await fetch(url);
@@ -27,30 +12,35 @@ async function apiGET(pm_element, pm_service, pm_function) {
     console.log(data.results);
 
     data.results.forEach((record) => {
-      pm_element.innerHTML += pm_function(record);
+      if (record.user_id==pm_user) {
+        pm_element.innerHTML += pm_function(record);
+      }
     });
   } catch (error) {
     console.log(error);
   }
 }
-function listResults(data) {
+function listPayments(data) {
   return `
-  <div>Service ID: ${data.service_id}</div>
-  <div>User ID(delete): ${data.user_id}</div>
-  <div>Amount: ${data.amount}</div>
   <div>Logo: (faltante)</div>
+  <div>Service ID: ${data.service_id}</div>
+  <div>Fecha pago: ${data.payment_date}</div>
+  <div>Amount: ${data.amount}</div>
+  <div>User ID(delete): ${data.user_id}</div>
   <hr></hr>
   `;
 }
-function listResults2(data) {
+function listPaymentsExpired(data) {
   return `
+  <div>Logo: (faltante)</div>
+  <div>Service ID: ${data.service_id}</div>
+  <div>Fecha pago: (faltante)</div>
+  <div>Amount: ${data.amount}</div>
   <div>Amount fee: ${data.amount_fee}</div>
   <div>User ID(delete): ${data.user_id}</div>
-  <div>Amount: ${data.amount}</div>
-  <div>Logo: (faltante)</div>
   hola
   <hr></hr>
   `;
 }
-apiGET(html_payments, 'payments', listResults);
-apiGET(html_expired, 'payments-expired', listResults2);
+apiGET(html_payments, 'payments', listPayments, 1);
+apiGET(html_expired, 'payments-expired', listPaymentsExpired, 1);
