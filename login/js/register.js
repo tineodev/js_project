73971f66b8f6c90019700bf71be3f988a4137405
tineodev <tineo.dev@gmail.com>
2022-12-28 10:35:13 -0500

    Fix type input

---
+++ login/js/register.js
@@ -0,0 +1,43 @@
+const htmlForm = document.querySelector("#form_register");
+
+const input = document.createElement("input");
+input.type = "hidden";
+input.name = "csrfmiddlewaretoken";
+input.value = "aaaaa";
+htmlForm.appendChild(input);
+
+htmlForm.onsubmit = async function (event) {
+  event.preventDefault();
+
+  const first_name = htmlForm.elements.first_name.value;
+  const last_name = htmlForm.elements.last_name.value;
+  const username = htmlForm.elements.username.value;
+  const password1 = htmlForm.elements.password1.value;
+  const password2 = htmlForm.elements.password2.value;
+  const email = htmlForm.elements.email.value;
+  const csrf_token = htmlForm.elements.csrfmiddlewaretoken.value;
+
+  const user = {
+    first_name: first_name,
+    last_name: last_name,
+    username: username,
+    email: email,
+    password1: password1,
+    password2: password2,
+    csrfmiddlewaretoken: csrf_token,
+  };
+
+  try {
+    const options = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(user),
+    };
+    console.log(JSON.stringify(user));
+    const response = await fetch("http://127.0.0.1:8000/register/", options);
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
